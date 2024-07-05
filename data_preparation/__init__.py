71236f4ea085da1db07b9354ca896e94d1959409
matfe <ironmakeo@gmail.com>
2024-07-05 22:58:51 +0200

    Aggiorna commento nel modulo

--- data_preparation/__init__.py
+++ data_preparation/__init__.py
@@ -1,9 +1,11 @@
 """
 Il pacchetto data_preparation contiene i moduli per preparare i dati per le analisi successive.
 
-Lista dei moduli di questo pacchetto:
-- loader: carica i dati da dataset
-- validator: valida i dati
-- cleaner: pulisce e normalizza i dati
-- explorer: esplora le caratteristiche dei dati
+Moduli:
+    loader: carica i dati dal dataset
+    validator: valida i dati
+    cleaner: pulisce e normalizza i dati
+    explorer: esplora le caratteristiche dei dati
 """
+
+from .loader import DataLoader
