44dc410efb5c20e4fbcb0fd4a11994f79078b5c6
matfe <ironmakeo@gmail.com>
2024-07-05 23:02:02 +0200

    Modifica main per testare il dataframe restituito da DataLoader

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-# This is a sample Python script.
+import os
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StringType
 
+from data_preparation import DataLoader
+from util import SchemaBuilder
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# Configurazione di una sessione Spark
+spark = SparkSession.builder \
+    .master("local[*]") \
+    .appName("MyApp") \
+    .config("spark.local", "local") \
+    .config("spark.driver.host", "localhost") \
+    .getOrCreate()
+def main():
+    schema = SchemaBuilder().build_schema()
+    dataset_df = DataLoader(spark, schema).load_data()
 
+    print(f'numero di elementi del dataset: {dataset_df.count()}')
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
