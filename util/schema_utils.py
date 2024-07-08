4ddda082f8782e7f0428c5803a4bfceec8716c41
matfe <ironmakeo@gmail.com>
2024-07-08 21:41:24 +0200

    Aggiorna lo SchemaBuilder per leggere il tipo dato dei campi

--- util/schema_utils.py
+++ util/schema_utils.py
@@ -1,6 +1,6 @@
 import json
 
-from pyspark.sql.types import StructField,StructType,StringType
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DoubleType
 from .config_loader import ConfigLoader
 
 class SchemaBuilder:
@@ -11,7 +11,19 @@ class SchemaBuilder:
         schema_config = ConfigLoader(schema_config_path).load_config()
         fields = []
         for field in schema_config['fields']:
-            field = StructField(field['name'], StringType(), nullable=field['nullable'])
+            data_type = self.get_field_type(field)
+            field = StructField(field['name'], data_type, nullable=field['nullable'])
             fields.append(field)
-
         return StructType(fields)
+
+    def get_field_type(self, field):
+        field_type = field['type']
+        if field_type == 'string':
+            data_type = StringType()
+        elif field_type == 'integer':
+            data_type = IntegerType()
+        elif field_type == 'double':
+            data_type = DoubleType()
+        else:
+            data_type = StringType()
+        return data_type
