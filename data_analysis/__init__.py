824c66522f992a0565f2cf014e887028003eeae5
matfe <ironmakeo@gmail.com>
2024-07-19 17:12:29 +0200

    Aggiungi importazione relativa per AccidentAnalysis in __init__.py di data_analysis

--- data_analysis/__init__.py
+++ data_analysis/__init__.py
@@ -1,6 +1,7 @@
 """
 Il pacchetto data_analysis contiene i moduli responsabili dell'analisi dei dati
 """
+from .accident_analysis import AccidentAnalysis
 from .accident_geo_analysis import AccidentGeoAnalysis
 
 
