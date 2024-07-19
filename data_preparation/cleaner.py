be9e5ac2ffc967f9bf07e9289a9a08ce774eeb19
Dario <darionapoli67@gmail.com>
2024-07-19 09:54:21 +0200

    Rifattorizza la classe DataCleaner per leggere i dati dal file di configurazione

--- data_preparation/cleaner.py
+++ data_preparation/cleaner.py
@@ -2,76 +2,98 @@ from pyspark.sql.functions import col, count, when, lower, trim, to_timestamp, s
 
 from pyspark.sql import DataFrame
 
+from util import ConfigLoader
 
 class DataCleaner:
     """
-
+    Classe per la pulizia e la trasformazione dei dati di un DataFrame.
     """
-    def __init__(self, dataset_df: DataFrame):
+    def __init__(self, config_path='./config/application.json'):
+        # Carica le configurazioni dal file di configurazione
+        self.config = ConfigLoader(config_path).load_config()
+        self.qualitative_variables = self.config['variables']['qualitative']
+        self.quantitative_variables = self.config['variables']['quantitative']
+        self.geographic_variables = self.config['variables']['geographic']
+        self.drop_variables = self.config['drop']
+        self.rename_variables = self.config['rename']['old_new']
+
+    def clean_data(self, dataset_df):
+        """
+        Pulisce e trasforma completa del DataFrame.
+
+        Richiama i metodi per rinominare le colonne, sostituire i valori nulli, normalizzare il testo delle colonne e rimuovere le colonne specificate.
+
+        :return: Il DataFrame aggiornato
         """
 
-        :param dataset_df:
         """
-        self.dataset_df = dataset_df
-
-    def clean_data(self):
+        Seleziona le prime 37 colonne: nei CSV più recenti è presente un ulteriore colonna vuota da ignorare
         """
-
-        :return:
+        dataset_df = dataset_df.select(dataset_df.columns[:36])
         """
-        # seleziona le prime 37 colonne: nei CSV più recenti è presente un ulteriore colonna vuota da ignorare
-        self.dataset_df = self.dataset_df.select(self.dataset_df.columns[:36])
-        # elimina righe che hanno gli stessi valori in ogni colonna
-        self.dataset_df = self.dataset_df.drop_duplicates()
-        # ridenomina alcune colonne usando il CamelCase
-        self.dataset_df = self.dataset_df.withColumnRenamed("STRADA1", "Strada1") \
-            .withColumnRenamed("STRADA2", "Strada2") \
-            .withColumnRenamed("Strada02", "Strada02") \
-            .withColumnRenamed("particolaritastrade", "ParticolaritaStrade") \
-            .withColumnRenamed("NUM_FERITI", "NumFeriti") \
-            .withColumnRenamed("NUM_FERITI", "NumFeriti") \
-            .withColumnRenamed("NUM_RISERVATA", "NumRiservata") \
-            .withColumnRenamed("NUM_MORTI", "NumMorti") \
-            .withColumnRenamed("NUM_ILLESI", "NumIllesi") \
-            .withColumnRenamed("Tipolesione", "TipoLesione")
-        # Chilometrica e DaSpecificare nel dataset contano rispettivamente 742382 e 943868 valori nulli, DecedutoDopo 921367 nulli
-        # Strada2 conta 716080 nulli: Strada02 conta 257297 nulli hanno stessi valori, si può droppare Strada2
-        self.dataset_df = self.remove_unnecessary_columns(["Strada2", "Chilometrica", "DaSpecificare", "DecedutoDopo"])
-        self.dataset_df = self.handle_missing_values(["Localizzazione1"], "")
-        self.dataset_df = self.normalize_text_data(["ParticolaritaStrade"])
-
-        self.dataset_df = self.dataset_df.withColumn('DataOraIncidente', substring(col('DataOraIncidente'), 1, 16))
-        self.dataset_df = self.dataset_df.withColumn('DataOraIncidente', to_timestamp(col('DataOraIncidente'), 'dd/MM/yyyy HH:mm'))
-
-
-        return self.dataset_df
-
-    def remove_unnecessary_columns(self, columns_to_remove):
+        Elimina righe che hanno gli stessi valori in ogni colonna
         """
-
-        :param columns_to_remove:
-        :return:
+        dataset_df = dataset_df.drop_duplicates()
+        dataset_df = self.rename_columns(dataset_df)
         """
-        return self.dataset_df.drop(*columns_to_remove)
-
-    def handle_missing_values(self, columns, fill_value=None):
+        Alcune colonne sono state rimosse perchè contengono un numero elevato di valori nulli.
+        
+        Ad esempio:
+        Chilometrica e DaSpecificare hanno rispettivamente 742382 e 943868 valori nulli, mentre DecedutoDopo ha 921367 nulli
+        Strada2 ha 716080 nulli e Strada02 ha 257297 nulli: hanno stessi valori, quindi si può droppare Strada2
         """
-
-        :param columns:
-        :param fill_value:
-        :return:
+        dataset_df = self.remove_unnecessary_columns(dataset_df, self.drop_variables)
+        dataset_df = self.handle_missing_values(dataset_df, self.quantitative_variables, 0)
+        dataset_df = self.normalize_text_data(dataset_df, self.qualitative_variables)
+        dataset_df = self.normalize_text_data(dataset_df, self.geographic_variables)
         """
-        return self.dataset_df.na.fill(fill_value, subset=columns)
-
-    def normalize_text_data(self, text_columns):
+        Tronca la colonna 'DataOraIncidente' ai primi 16 caratteri, convertendola in formato timestamp 'dd/MM/yyyy HH
         """
+        dataset_df = dataset_df.withColumn('DataOraIncidente', substring(col('DataOraIncidente'), 1, 16))  \
+                               .withColumn('DataOraIncidente', to_timestamp(col('DataOraIncidente'), 'dd/MM/yyyy HH:mm'))
 
-        :param text_columns:
-        :return:
+        return dataset_df
+
+    def rename_columns(self, dataset_df):
+        """
+        Rinomina le colonne del DataFrame in base a una mappatura specificata.
+
+        :param dataset_df: Il DataFrame da elaborare.
+        :return: Il DataFrame con le colonne rinominate.
+        """
+        for old_name, new_name in self.rename_variables.items():
+            dataset_df = dataset_df.withColumnRenamed(old_name, new_name)
+        return dataset_df
+
+    def remove_unnecessary_columns(self, dataset_df, columns_to_remove):
+        """
+        Rimuove un elenco di colonne specificate dal DataFrame.
+
+        :param dataset_df: Il DataFrame da elaborare
+        :param columns_to_remove: Lista delle colonne da rimuovere
+        :return: DataFrame aggiornato
+        """
+        return dataset_df.drop(*columns_to_remove)
+
+    def handle_missing_values(self, dataset_df, columns, fill_value=None):
+        """
+        Esegue la sostituzione dei valori nulli con fill_value in un elenco di colonne del DataFrame.
+
+        :param dataset_df: Il DataFrame da elaborare
+        :param columns: Lista delle colonne in cui sostituire i valori nulli
+        :param fill_value: Il valore con cui sostituire i valori nulli
+        :return: DataFrame aggiornato
+        """
+        return dataset_df.na.fill(fill_value, subset=columns)
+
+    def normalize_text_data(self, dataset_df, text_columns):
+        """
+        Esegue operazioni di normalizzazione sul testo di una lista di colonne del DataFrame.
+
+        :param dataset_df: Il DataFrame da elaborare
+        :param text_columns: Lista delle colonne di testo da normalizzare
+        :return: DataFrame aggiornato
         """
         for text_col in text_columns:
-            self.dataset_df = self.dataset_df.withColumn(text_col, lower(trim(col(text_col))))
-        return self.dataset_df
-
-
-
+            dataset_df = dataset_df.withColumn(text_col, lower(trim(col(text_col))))
+        return dataset_df
