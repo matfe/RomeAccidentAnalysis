3ea7d41ef99f2ce7547670e738c88148c0b1e4ed
Dario <darionapoli67@gmail.com>
2024-07-19 09:56:36 +0200

    Rifattorizza la classe DataAnalysisExplorer per leggere i dati dal file di configurazione

--- data_preparation/explorer.py
+++ data_preparation/explorer.py
@@ -1,37 +1,46 @@
 from pyspark.sql.functions import count, when, isnan, col
 
 from util import AnalysisOutputSaver
+from util import ConfigLoader
 
 class DataAnalysisExplorer:
     """
     Classe per eseguire l'analisi esplorativa dei dati.
     """
 
-    def __init__(self, dataframe):
-        self.dataframe = dataframe
+    def __init__(self, config_path='./config/application.json'):
+        # Carica le configurazioni dal file di configurazione
+        self.config = ConfigLoader(config_path).load_config()
+        self.quantitative_variables = self.config['variables']['qualitative']
 
-    def explore_data(self):
-        self.describe_data()
-        self.count_missing_values()
-        self.show_unique_values(["NaturaIncidente","ParticolaritaStrade","TipoStrada","FondoStradale"])
+    def explore_data(self, dataframe):
+        """
+        Esegue un insieme di task di analisi per esplorare i dati.
 
-    def describe_data(self):
+        I risultati delle analisi vengono salvati in diversi file HTML
+        """
+        self.describe_data(dataframe)
+        self.count_missing_values(dataframe)
+        self.show_unique_values(dataframe, self.quantitative_variables)
+
+    def describe_data(self, dataframe):
         """
         Calcola e salva in un file CSV le statistiche descrittive per il dataframe.
         """
-        dataframe_description = self.dataframe.describe()
+        dataframe_description = dataframe.describe()
         AnalysisOutputSaver().save_html(dataframe_description, "dataframe_description")
 
-    def count_missing_values(self):
+    def count_missing_values(self, dataframe):
         """
         Conta il numero di valori mancanti per ogni colonna nel dataframe.
         """
-        missing_values = self.dataframe.select([count(when(isnan(c) | col(c).isNull(), c)).alias(c) for c in self.dataframe.columns])
+        missing_values = dataframe.select([count(when(col(c).isNull(), c)).alias(c) for c in dataframe.columns])
         AnalysisOutputSaver().save_html(missing_values, "missing_values")
 
-    def show_unique_values(self, variables):
+    def show_unique_values(self, dataframe, variables):
         """
-        Mostra i valori unici per una colonna specifica.
+        Elenca i valori unici per una colonna specifica.
         """
-        unique_values = self.dataframe.select(variables[0]).distinct()
-        AnalysisOutputSaver().save_html(unique_values, "unique_values")
+        for variable in variables:
+            unique_values = dataframe.select(variable).distinct()
+            AnalysisOutputSaver().save_html(unique_values, f'unique_values_{variable}')
