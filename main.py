259a2a5a0fa18f0908eabaa65a927b901da365da
matfe <ironmakeo@gmail.com>
2024-07-22 21:01:30 +0200

    Modifica main per creare e analizzare i grafi AccidentCrossroadsGraph e AccidentPeopleGraph

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StringType
 
 from util import SchemaBuilder
-
+from util import AnalysisOutputSaver
 from data_preparation import DataLoader
 from data_preparation import DataCleaner
 from data_preparation import DataAnalysisExplorer
 from data_analysis import AccidentGeoAnalysis
 from data_analysis import AccidentAnalysis
-from util import AnalysisOutputSaver
+from graph_analysis import GraphAnalysis
+from graph_analysis import AccidentPeopleGraph
+from graph_analysis import AccidentCrossroadsGraph
 
 # Configurazione di una sessione Spark
 spark = SparkSession.builder \
@@ -18,6 +20,7 @@ spark = SparkSession.builder \
     .appName("MyApp") \
     .config("spark.local", "local") \
     .config("spark.driver.host", "localhost") \
+    .config("spark.jars.packages", "graphframes:graphframes:0.8.2-spark3.0-s_2.12") \
     .getOrCreate()
 
 def main():
@@ -30,6 +33,9 @@ def main():
 
     # Pulisce il dataset.
     cleaned_dataset_df = DataCleaner().clean_data(dataset_df)
+
+    # Memorizza il DataFrame in memoria.
+    cleaned_dataset_df = cleaned_dataset_df.cache()
 
     # Esplora i dati puliti per ottenere informazioni preliminari e statistiche descrittive.
     DataAnalysisExplorer().explore_data(cleaned_dataset_df)
@@ -48,13 +54,24 @@ def main():
     # Crea e salva le mappe geografiche degli incidenti utilizzando il dataset pulito.
     AccidentGeoAnalysis().plot_maps(cleaned_dataset_df)
 
-    """
     # Ottiene le combinazioni demografiche da analizzare.
     demographic_combinations = AccidentAnalysis().get_demographic_combinations_for_analysis()
 
     # Salva la distribuzione dei dati demografici nel dataset pulito.
     AnalysisOutputSaver().save_plot_distribution(cleaned_dataset_df, demographic_combinations)
-    """
+
+    # Crea il grafo di incroci stradali con incidenti.
+    accidentCrossroadsGraph, nameAccidentCrossroadsGraph = AccidentCrossroadsGraph(cleaned_dataset_df).graph()
+
+    # Crea il grafo di persone coinvolte in incidenti.
+    accidentPeopleGraph, nameAccidentPeopleGraph = AccidentPeopleGraph(cleaned_dataset_df).graph()
+
+    # Analizza il grafo di incidenti avvenuti agli incroci stradali.
+    GraphAnalysis.analyze(accidentCrossroadsGraph, nameAccidentCrossroadsGraph)
+
+    # Analizza il grafo di persone coinvolte in incidenti.
+    GraphAnalysis.analyze(accidentPeopleGraph, nameAccidentPeopleGraph, sampleForAdvancedAnalysis=True)
+
 
 if __name__ == '__main__':
     main()
