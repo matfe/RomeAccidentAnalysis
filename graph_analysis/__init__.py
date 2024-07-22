8323c6bfb86a25d195c445ee8dd2c761ebc4e727
matfe <ironmakeo@gmail.com>
2024-07-22 20:55:47 +0200

    Aggiungi importazione relativa per AccidentCrossroadsGraph e AccidentPeopleGraph in __init__.py di graph_analysis

--- graph_analysis/__init__.py
+++ graph_analysis/__init__.py
@@ -1,4 +1,8 @@
 """
-Il pacchetto graph_analysis
-
+Il pacchetto graph_analysis contiene i moduli per analizzare un DataFrame rappresentato come un grafo di tipo GraphFrame
 """
+from .graph_analysis import GraphAnalysis
+from .accident_crossroads_graph import AccidentCrossroadsGraph
+from .accident_people_graph import AccidentPeopleGraph
+
+
