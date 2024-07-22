6d44849509bd900cae7edc05087cc46a9049d111
matfe <ironmakeo@gmail.com>
2024-07-22 16:33:30 +0200

    Aggiorna la classe PathBuilder con commenti e implementazione del metodo get_hdfs_path

--- util/path_utils.py
+++ util/path_utils.py
@@ -11,13 +11,26 @@ class PathBuilder:
         self.csv_path = self.get_csv_path()
 
     def get_os_path(self):
+        """
+        Restituisce il percorso assoluto della directory in cui si trova lo script corrente.
+        """
         return os.path.abspath(os.path.dirname(__file__))
 
     def get_data_path(self):
+        """
+        Restituisce il percorso completo alla directory del dataset.
+        """
         return os.path.join(self.os_path, self.base_path)
 
     def get_csv_path(self):
+        """
+        Restituisce il percorso per accedere ai file CSV.
+        """
         return os.path.join(f'file:///{self.data_path}', '*', '*.csv')
 
     def get_hdfs_path(self):
-        raise MethodNotImplementedError
+        """
+        Restituisce il percorso per accedere ai file CSV memorizzati in HDFS.
+        """
+        return os.path.join(f'hdfs://namenode/{self.data_path}', '*', '*.csv')
+
