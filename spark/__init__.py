dc1c812f85507966d4ab2b5048a2cd89707f5df7
matfe <ironmakeo@gmail.com>
2024-07-23 19:26:38 +0200

    Aggiungi importazione relativa per SchemaBuilder e SparkSessionBuilder in __init__.py di spark

---
+++ spark/__init__.py
@@ -0,0 +1,6 @@
+"""
+Il pacchetto Spark contiene moduli per la creazione e la gestione di sessioni Spark e dello schema di dati.
+"""
+
+from .schema import SchemaBuilder
+from .session import SparkSessionBuilder
