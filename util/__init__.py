d7d1de8188043c8107b4d520d51d15affe1545aa
matfe <ironmakeo@gmail.com>
2024-07-23 19:27:16 +0200

    Rimuovi importazione relativa per modulo rimosso in __init__.py di util

--- util/__init__.py
+++ util/__init__.py
@@ -1,13 +1,7 @@
 """
 Il pacchetto util contiene i moduli che forniscono funzioni comuni di utilità
-
-Moduli:
-    schema_utils: gestisce lo schema configurato
-    config_loader: carica file di configurazione
-    path_utils: gestisce e costruisce percorsi di file e directory
 """
 
-from .schema_utils import SchemaBuilder
 from .config_loader import ConfigLoader
 from .path_utils import PathBuilder
 from .analysis_output_utils import AnalysisOutputSaver
