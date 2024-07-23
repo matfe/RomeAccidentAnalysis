3a2f55d6c7c11a50cd950a023abc9ec566e86250
matfe <ironmakeo@gmail.com>
2024-07-23 19:25:15 +0200

    Aggiungi il modulo schema.py per creare e gestire lo schema dei dati

--- spark/schema.py
+++ spark/schema.py
@@ -1,13 +1,21 @@
 import json
 
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DoubleType
-from .config_loader import ConfigLoader
+
+from util import ConfigLoader
+
 
 class SchemaBuilder:
     """
-    Classe per costruire lo schema configurato in file JSON
+    Classe per creare e gestisce lo schema configurato
     """
     def build_schema(self,  schema_config_path = "./config/schema_config.json"):
+        """
+         Costruisce uno schema per i dati utilizzando una configurazione specificata.
+
+        :param schema_config_path: Il percorso del file di configurazione dello schema.
+        :return: Un oggetto StructType che rappresenta lo schema costruito.
+        """
         schema_config = ConfigLoader(schema_config_path).load_config()
         fields = []
         for field in schema_config['fields']:
@@ -17,6 +25,12 @@ class SchemaBuilder:
         return StructType(fields)
 
     def get_field_type(self, field):
+        """
+           Determina il tipo di dato per un campo specificato.
+
+           :param field: Un dizionario che rappresenta il campo, contenente una chiave 'type' il cui valore che descrive il tipo del campo.
+           :return: Un oggetto tipo che rappresenta il tipo di dato appropriato (StringType, IntegerType, DoubleType).
+           """
         field_type = field['type']
         if field_type == 'string':
             data_type = StringType()
